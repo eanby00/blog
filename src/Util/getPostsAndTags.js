b4dc1ab0c329814751ecbf23e631539eb1a8716d
eanby00 <wjdwnstjd00@gmail.com>
2024-08-23 14:47:38 +0900

    base64 decoder 구현

--- src/Util/getPostsAndTags.js
+++ src/Util/getPostsAndTags.js
@@ -17,6 +17,11 @@ export const getPostsAndTags = () => {
     tags.add(tag);
   };
 
+  const decodeBase64 = (raw) => {
+    const utf8Array = Uint8Array.from(atob(raw), (m) => m.charCodeAt(0));
+    return new TextDecoder().decode(utf8Array);
+  };
+
   const addPost = (response, tag) => {
     posts.push({
       ...response.data,
