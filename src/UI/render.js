4c782ef72ab5c409c177bf194907fd5d309c1b07
eanby00 <wjdwnstjd00@gmail.com>
2024-10-15 16:13:40 +0900

    tag 선택 로직 수정

--- src/UI/render.js
+++ src/UI/render.js
@@ -1,5 +1,6 @@
 export const render = (posts, tags) => {
   let filteredPosts = [...posts];
+  let selectedTag = "";
 
   const renderHeader = () => {
     const mobileTagIcon = document.querySelector(".mobile-menu");
@@ -17,19 +18,37 @@ export const render = (posts, tags) => {
     backdrop.addEventListener("click", toggleMenu);
   };
 
+  const selectTag = (tagElement, tag) => {};
+
   const renderTags = (tags) => {
     const tagContainer = document.querySelector(".tag-container");
     const tagTemplate = document.querySelector(".template-tag");
+    tagContainer.addEventListener("click", (event) => {
+      if (!event.target.tagName === "DIV") {
+        return;
+      }
+
+      Array.from(event.target.closest("nav").children).forEach((tag) => {
+        tag.classList.remove("tag-open");
+      });
+      if (selectedTag === event.target.textContent) {
+        filteredPosts = [...posts];
+      } else {
+        filteredPosts = posts.filter(
+          (post) => post.tag === event.target.textContent
+        );
+        selectedTag = event.target.textContent;
+        event.target.classList.add("tag-open");
+      }
+      renderPosts(filteredPosts);
+      console.log(filteredPosts);
+    });
 
     tags.forEach((tag) => {
       const tagElement = document
         .importNode(tagTemplate.content, true)
         .querySelector("div");
       tagElement.textContent = tag;
-      tagElement.addEventListener("click", () => {
-        filteredPosts = posts.filter((post) => post.tag === tag);
-        renderPosts(filteredPosts);
-      });
 
       tagContainer.append(tagElement);
     });
