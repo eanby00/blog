5e0063ad1d9b06a91aaf5d497b6758ec554e27d5
eanby00 <wjdwnstjd00@gmail.com>
2024-10-17 16:53:44 +0900

    post 렌더링 로직 보강

--- src/UI/render.js
+++ src/UI/render.js
@@ -69,6 +69,7 @@ export const render = (posts, tags) => {
   };
 
   const renderPosts = (posts) => {
+    postContainer.replaceChildren();
     posts.forEach((post) => {
       const postElement = document
         .importNode(postTemplate.content, true)
@@ -76,7 +77,7 @@ export const render = (posts, tags) => {
       postElement.querySelector("h2").textContent = post.title;
       postElement.querySelector(".post-date").textContent = post.date;
       postElement.querySelector(".post-description").textContent =
-        post.description;
+        post.description || `${post.title}에 관한 포스트`;
 
       postContainer.append(postElement);
     });
