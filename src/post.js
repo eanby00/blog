c1a068762d09738ffc5a411f75d2e43d1bd65059
eanby00 <wjdwnstjd00@gmail.com>
2024-10-22 13:54:11 +0900

    포스트 페이지 데이터 전달 확인

--- src/post.js
+++ src/post.js
@@ -1,8 +1,13 @@
 console.log("Test");
 const url = new URL(location.href);
 const queryParams = url.searchParams;
-const id = queryParams.get("id");
-const test = queryParams.get("test");
+const title = queryParams.get("title");
+const tag = queryParams.get("tag");
 console.log(url);
 console.log(queryParams);
-console.log(id, test);
+console.log(title, tag);
+
+const posts = JSON.parse(sessionStorage.getItem("posts"));
+console.log(posts);
+const post = posts.filter((post) => post.title === title);
+console.log(post);
