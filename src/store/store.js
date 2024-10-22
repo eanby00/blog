7ba155e672a40ff7c98fa5bc8f5f9fec0350c9ed
eanby00 <wjdwnstjd00@gmail.com>
2024-10-22 14:14:12 +0900

    sessionStorage 저장 방식 변경

--- src/store/store.js
+++ src/store/store.js
@@ -6,17 +6,36 @@ const getStorage = (key) => {
   return sessionStorage.getItem(key);
 };
 
-export const saveData = (posts, tags) => {
-  setStorage("posts", posts);
-  setStorage("tags", tags);
+const getKey = (index) => {
+  return sessionStorage.key(index);
 };
 
-export const loadData = () => {
-  const posts = JSON.parse(getStorage("posts"));
-  const tags = JSON.parse(getStorage("tags"));
-  return { posts, tags };
+const getPosts = () => {
+  const posts = [];
+  for (let i = 0; i < sessionStorage.length; ++i) {
+    const key = getKey(i);
+    if (key !== "tags") {
+      posts.push(JSON.parse(getStorage(key)));
+    }
+  }
+  return posts;
+};
+
+const getTags = () => {
+  return JSON.parse(getStorage("tags"));
 };
 
 export const hasData = () => {
   return !(sessionStorage.length === 0);
+};
+
+export const saveData = (posts, tags) => {
+  posts.forEach((post) => {
+    setStorage(post.title, post);
+  });
+  setStorage("tags", tags);
+};
+
+export const loadData = () => {
+  return { posts: getPosts(), tags: getTags() };
 };
