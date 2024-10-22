4a9a80c68e2a79a32329759b53464d244b9cd599
eanby00 <wjdwnstjd00@gmail.com>
2024-10-22 13:44:21 +0900

    포스트 페이지 확인

--- src/post.js
+++ src/post.js
@@ -1,1 +1,8 @@
 console.log("Test");
+const url = new URL(location.href);
+const queryParams = url.searchParams;
+const id = queryParams.get("id");
+const test = queryParams.get("test");
+console.log(url);
+console.log(queryParams);
+console.log(id, test);
