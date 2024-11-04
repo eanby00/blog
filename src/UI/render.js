d906975dfbb5f6d9a5e830cd1942a44f9c7484af
eanby00 <wjdwnstjd00@gmail.com>
2024-11-04 15:41:19 +0900

    리팩토링 - renderLayout으로 통합

--- src/UI/render.js
+++ src/UI/render.js
@@ -1,10 +1,10 @@
-import { renderHeader } from "./renderHeader";
+import { renderLayout } from "./renderLayout";
 import { removeLoadingSpinner } from "./renderLoadingSpinner";
 import { renderPosts } from "./renderPosts";
 import { renderTags } from "./renderTags";
 
 export const render = (posts, tags) => {
-  renderHeader();
+  renderLayout();
   renderTags(posts, tags);
   renderPosts(posts);
   removeLoadingSpinner();
---
+++ src/UI/renderLayout.js
@@ -0,0 +1,42 @@
+import { GITHUB_API } from "../constants/API";
+import { $ } from "../Util/Helper";
+import { renderHeader } from "./renderHeader";
+
+const renderGithubIcon = (url) => {
+  const githubIcon = $("footer a");
+  githubIcon.href = url ?? GITHUB_API.URL;
+  githubIcon.classList.remove("close");
+};
+
+const observeHeader = (targetEl, entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      targetEl.classList.remove("display");
+    } else {
+      targetEl.classList.add("display");
+    }
+  });
+};
+
+const renderUpToTop = () => {
+  const upToTopButton = $(".upToTop");
+  upToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
+
+  const header = $(".main-header");
+  const observer = new IntersectionObserver(
+    observeHeader.bind(null, upToTopButton)
+  );
+  observer.observe(header);
+};
+
+export const renderLayout = (url) => {
+  renderHeader();
+  renderGithubIcon(url);
+  renderUpToTop();
+};
