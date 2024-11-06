4b2bd73cfc45a540f0b1ef215b9ebc97845ac788
eanby00 <wjdwnstjd00@gmail.com>
2024-11-06 15:35:39 +0900

    decodeBase64 테스트 코드 작성 및 오류 수정

--- src/Util/decodeBase64.js
+++ src/Util/decodeBase64.js
@@ -1,4 +1,8 @@
 export const decodeBase64 = (raw) => {
-  const utf8Array = Uint8Array.from(atob(raw), (m) => m.charCodeAt(0));
-  return new TextDecoder().decode(utf8Array);
+  try {
+    const utf8Array = Uint8Array.from(atob(raw), (m) => m.charCodeAt(0));
+    return new TextDecoder().decode(utf8Array);
+  } catch {
+    return null;
+  }
 };
