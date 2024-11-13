0e2b6dd7dc7c9ca7c109239ba38ff9400e28f080
eanby00 <wjdwnstjd00@gmail.com>
2024-11-13 16:59:45 +0900

    generateID, sortArray, deduplication, formatDate 테스트 코드 작성

---
+++ test/Util/Helper.test.js
@@ -0,0 +1,97 @@
+import {
+  deduplication,
+  formatDate,
+  generateID,
+  sortArray,
+} from "../../src/Util/Helper";
+
+describe("generateID 체크", () => {
+  test("'test'를 title로 하였을 경우", () => {
+    const test = generateID("test");
+    expect(test.slice(0, 3)).toEqual("1c0");
+  });
+
+  test("'test10'를 title로 하였을 경우", () => {
+    const test = generateID("test10");
+    expect(test.slice(0, 3)).toEqual("221");
+  });
+});
+
+describe("sortArray 체크", () => {
+  const answerArray = [
+    { id: 3, date: 5 },
+    { id: 4, date: 4 },
+    { id: 5, date: 3 },
+    { id: 2, date: 2 },
+    { id: 1, date: 1 },
+  ];
+
+  test("정렬되지 않은 배열을 정렬할 경우", () => {
+    const testArray = [
+      { id: 1, date: 1 },
+      { id: 2, date: 2 },
+      { id: 3, date: 5 },
+      { id: 4, date: 4 },
+      { id: 5, date: 3 },
+    ];
+    const test = sortArray(testArray);
+    expect(test).toEqual(answerArray);
+  });
+
+  test("내림차순으로 정렬된 배열을 정렬할 경우", () => {
+    const testArray = [
+      { id: 3, date: 5 },
+      { id: 4, date: 4 },
+      { id: 5, date: 3 },
+      { id: 2, date: 2 },
+      { id: 1, date: 1 },
+    ];
+    const test = sortArray(testArray);
+    expect(test).toEqual(answerArray);
+  });
+
+  test("오름차순으로 정렬된 배열을 정렬할 경우", () => {
+    const testArray = [
+      { id: 1, date: 1 },
+      { id: 2, date: 2 },
+      { id: 5, date: 3 },
+      { id: 4, date: 4 },
+      { id: 3, date: 5 },
+    ];
+    const test = sortArray(testArray);
+    expect(test).toEqual(answerArray);
+  });
+});
+
+describe("deduplication 체크", () => {
+  test("중복이 없는 경우", () => {
+    const test = deduplication([1, 2, 3, 4, 5]);
+    expect(test).toHaveLength(5);
+  });
+
+  test("중복이 있는 경우", () => {
+    const test = deduplication([1, 2, 3, 2, 1]);
+    expect(test).toHaveLength(3);
+  });
+
+  test("배열이 없는 경우", () => {
+    const test = deduplication();
+    expect(test).toHaveLength(0);
+  });
+});
+
+describe("formatDate 체크", () => {
+  test("달이 10보다 큰 경우", () => {
+    const testTime = "2024-11-13T07:40:42.303Z";
+    const test = formatDate(new Date(testTime));
+
+    expect(test).toEqual("2024.11.13.");
+  });
+
+  test("달이 10보다 작은 경우", () => {
+    const testTime = "2024-05-13T07:40:42.303Z";
+    const test = formatDate(new Date(testTime));
+
+    expect(test).toEqual("2024.05.13.");
+  });
+});
