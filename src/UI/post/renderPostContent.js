809acf83414fd4a3ab65af969d2c6fa142c8412c
eanby00 <wjdwnstjd00@gmail.com>
2024-11-20 15:56:24 +0900

    renderPostContent 테스트 코드 작성 및 코드 수정

--- src/UI/post/renderPostContent.js
+++ src/UI/post/renderPostContent.js
@@ -28,8 +28,11 @@ const renderImage = (images) => {
   imageElements.forEach((imageElement) => {
     imageElement.id = imageElement.src.split("/").pop().replaceAll("%20", " ");
     const image = images.find((image) => image.name === imageElement.id);
-    if (image)
+    if (image) {
       imageElement.src = `data:image/${image.type};base64, ${image.content}`;
+    } else {
+      imageElement.remove();
+    }
   });
 };
 
